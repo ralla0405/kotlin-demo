fca380df7f9df4c007b9464fd595757e9d6b2966
Seunghwan Jung <seunghwan.jung@epikar.com>
2025-09-10 18:13:24 +0900

    컬렉션 함수

---
+++ src/main/kotlin/kr/io/wolverine/kotlindemo/example/CollectionFunctionMain.kt
@@ -0,0 +1,17 @@
+package kr.io.wolverine.kotlindemo.example
+
+fun main() {
+    // filter, filterIndexed
+    // map, mapIndexed, mapNotNull
+
+    // all, none, any
+    // count, sortedBy, sortedByDescending, distinctBy
+    // first, firstOrNull, last, lastOrNull
+
+    // List를 Map으로
+    // groupBy, associateBy
+
+    // 중첩 컬레션 처리 -> flatMap
+
+    // List<List<Fruit>>를 List<Fruit>로 변경 -> flatten 평탄화
+}
